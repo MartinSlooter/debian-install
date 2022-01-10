ba21ba66bf70717b75b60589f8e88cee55f897f5
Martin Slooter <80220595+MartinSlooter@users.noreply.github.com>
2022-01-10 16:37:52 +0100

    Create vms.sh

---
+++ vms.sh
@@ -0,0 +1,19 @@
+#Check Virtualization Extension 
+egrep -c '(vmx|svm)' /proc/cpuinfo
+
+#Install QEMU and Virtual Machine Manager
+sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
+
+#Verify that Libvirtd service is started
+sudo systemctl status libvirtd.service
+
+#Start Default Network for Networking
+sudo virsh net-start default
+sudo virsh net-autostart default
+
+#Check status with:
+sudo virsh net-list --all
+
+#Add User to libvirt to Allow Access to VMs
+sudo adduser $user libvirt
+sudo adduser $user libvirt-qemu
